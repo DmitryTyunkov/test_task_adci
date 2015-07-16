772488fb65ec2306192ee5cf83f574480fa9eb4e
Dmitry <tyunkov.dima@gmail.com>
2015-07-17 03:32:04 +0600

    Add plugs for the fifth and sixth test task
    Signed-off-by: Dmitry <tyunkov.dima@gmail.com>

---
+++ testtask.ru/www/testtask6/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>Тестовое задание 5</title>
+	</head>
+	<body>
+	
+		<br>
+		<a href="../">На главную</a>
+	</body>
+</html>
