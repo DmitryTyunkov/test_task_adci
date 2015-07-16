b929c2227627e04b288e7df3f0d9481704dca2ee
Dmitry <tyunkov.dima@gmail.com>
2015-07-17 03:08:58 +0600

    Add seventh test task
    Signed-off-by: Dmitry <tyunkov.dima@gmail.com>

---
+++ testtask.ru/www/testtask7/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>Тестовое задание 7</title>
+	</head>
+	<body>
+		<div style="width:100%; float: left;"><table>
+			<tr><form name="selectionTables" action="" method="post">
+				<td align="right"><label for="table">Выбор тип цикла:</label></td>
+				<td><select style="width: 200px" name="tables" id="tables" required>
+					<option value="for">for</option>
+					<option value="while">while</option>
+					<option value="do-while">do while</option>
+					<option value="foreach">foreach</option>
+				</select></td>
+				<td><input type="submit" name="select" id="select" value="Выбор"></td></form>
+			</tr>
+		</table></div>
+		<?require_once("script/select.php");?>
+		<br>
+		<a href="../">На главную</a>
+	</body>
+</html>
---
+++ testtask.ru/www/testtask7/script/select.php
@@ -0,0 +1,85 @@
+<?php
+	function mulFor()
+	{
+		echo "<div style=\"width:100%; float: left;\">";
+		echo "<div style=\"width:100%; float: left;\">for</div>";
+		for($i=1;$i<=10;$i++)
+		{
+			echo "<div style=\"height:200px; width:100px; float: left;\">";
+			for($j=1;$j<=10;$j++)
+			{
+				$result = $i*$j;
+				echo $i."*".$j."=".$result."<br>";
+			}
+			echo "</div>";
+		}
+		echo "</div>";
+	}
+	function mulWhile()
+	{
+		echo "<div style=\"width:100%; float: left;\">";
+		echo "<div style=\"width:100%; float: left;\">while</div>";
+		$i=1;
+		while($i<=10)
+		{
+			$j=1;
+			echo "<div style=\"height:200px; width:100px; float: left;\">";
+			while($j<=10)
+			{
+				$result = $i*$j;
+				echo $i."*".$j."=".$result."<br>";
+				$j++;
+			}
+			$i++;
+			echo "</div>";
+		}
+		echo "</div>";
+	}
+	function mulDo()
+	{
+		echo "<div style=\"width:100%; float: left;\">";
+		echo "<div style=\"width:100%; float: left;\">do-while</div>";
+		$i=1;
+		do
+		{
+			$j=1;
+			echo "<div style=\"height:200px; width:100px; float: left;\">";
+			do
+			{
+				$result = $i*$j;
+				echo $i."*".$j."=".$result."<br>";
+				$j++;
+			}
+			while($j<=10);
+			$i++;
+			echo "</div>";
+		}
+		while($i<=10);
+		echo "</div>";
+	}
+	function mulForeach()
+	{
+		$numbers=array(1,2,3,4,5,6,7,8,9,10);
+		echo "<div style=\"width:100%; float: left;\">";
+		echo "<div style=\"width:100%; float: left;\">foreach</div>";
+		foreach($numbers as $i)
+		{
+			echo "<div style=\"height:200px; width:100px; float: left;\">";
+			foreach($numbers as $j)
+			{
+				$result = $i*$j;
+				echo $i."*".$j."=".$result."<br>";
+			}
+			echo "</div>";
+		}
+		echo "</div>";
+	}
+	if($_POST['tables']=='for')
+		mulFor();
+	if($_POST['tables']=='while')
+		mulWhile();
+	if($_POST['tables']=='do-while')
+		mulDo();
+	if($_POST['tables']=='foreach')
+		mulForeach();
+?>
