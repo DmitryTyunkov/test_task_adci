9ecd594cc7cca63d36dd8b0d58179f9f0cea43e4
Dmitry <tyunkov.dima@gmail.com>
2015-07-17 02:12:14 +0600

    Add second test task
    Signed-off-by: Dmitry <tyunkov.dima@gmail.com>

--- testtask.ru/www/index.php
+++ testtask.ru/www/index.php
@@ -5,7 +5,14 @@
 	<body>
 		<h1>Список тестовых заданий</h1>
 		<ul>
-			<li><a href="testtask1/index.php">Тестовое задание 1</li>
+			<li><a href="testtask1">Тестовое задание 1</li>
+			<li><a href="testtask2">Тестовое задание 2</li>
+			<li><a href="testtask3">Тестовое задание 3</li>
+			<li><a href="testtask4">Тестовое задание 4</li>
+			<li><a href="testtask5">Тестовое задание 5</li>
+			<li><a href="testtask6">Тестовое задание 6</li>
+			<li><a href="testtask7">Тестовое задание 7</li>
+			<li><a href="testtask8">Тестовое задание 8</li>
 		</ul>
 	</body>
 </html>
---
+++ testtask.ru/www/testtask2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>Тестовое задание 2</title>
+	</head>
+	<body>
+		<h2><ul>
+			<li ondblclick=parentNode.removeChild(this);>Item 1</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 2</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 3</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 4</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 5</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 6</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 7</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 8</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 9</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 10</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 11</li>
+			<li ondblclick=parentNode.removeChild(this);>Item 12</li>
+		</ul></h2>
+		<br>
+		<a href="../">На главную</a>
+	</body>
+</html>
