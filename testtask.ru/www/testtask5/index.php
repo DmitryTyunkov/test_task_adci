789352874a0f2142ec92be33ad32d051a99c5d0c
Dmitry <tyunkov.dima@gmail.com>
2015-07-17 22:47:02 +0600

    Add fifth test task
    Signed-off-by: Dmitry <tyunkov.dima@gmail.com>

--- testtask.ru/www/testtask5/index.php
+++ testtask.ru/www/testtask5/index.php
@@ -2,10 +2,57 @@
 <html>
 	<head>
 		<title>Тестовое задание 5</title>
+		<style  type="text/css">
+			h1{
+				margin: 5px;
+			}
+			.top{
+				color: darkgray;
+				border-bottom: 2px solid lightgrey;
+				margin-bottom: 5px;
+				float: left;
+				width: 100%;
+				font: bold 32px arial;
+			}
+			.main{
+				width: 100%;
+				overflow: auto;
+				float: left;
+			}
+			.sign{
+				width: 100%;
+				height: 100px;
+				background: rgba(0, 0, 0, 0.7);
+				color: white;
+				position: relative;
+				top: -103px;
+				margin: 0px;
+				float: left;
+			}
+			.q{
+				width: 100%;
+				float: left;
+				position: relative;
+				top: -103px;
+			}
+		</style>
 	</head>
 	<body>
 	
+		<div class="top">
+			<h1>Animaux<h1>
+		</div>
+		<div class="main">
+			<img src="DoctorWho_s5_2.jpg" alt="Foto" width="100%">
+			<div class="sign">
+				<img src="DoctorWho_TheDayOfTheDoctor.jpg" alt="Logo" align="left" border="3" vspace="10" hspace="10" height="74">
+				<p style="font: normal 48px arial; margin-top: 5px; margin-bottom: 0px">Le Lion</p>
+				<p style="font: normal 28px arial; margin-top: 0px; margin-bottom: 0px">par Zorgk</p>
+			</div>
+		</div>
+	<div class="q">
 		<br>
 		<a href="../">На главную</a>
+	</div>
 	</body>
 </html>
